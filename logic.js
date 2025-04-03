77785574dc68d8976527f5967926b74604a619e7
Felipe <moralesfelipe123@hotmail.com>
2025-04-03 00:24:33 -0300

    cambio de api

--- logic.js
+++ logic.js
@@ -1,4 +1,3 @@
-
 let btn = document.getElementById("shorten");
 
 btn.addEventListener('click', short);
@@ -8,30 +7,25 @@ async function short() {
     let longURL = document.getElementById("longurl").value;
     let shortURL = document.getElementById("shorturl");
 
-    // Crear el cuerpo de la solicitud con la URL larga y la expiración
+    // Crear el cuerpo de la solicitud con la URL larga
     const inputBody = JSON.stringify({
-        url: longURL,
-        expiry: "5m" // O personaliza el tiempo de expiración
+        url: longURL
     });
-    //manyapis.com
-    const headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'x-api-key': 'sk_3c08fafe2456419ab3d9f8cc4e2d8fa8' // Reemplaza 'API_KEY' con tu clave real de API
-    };
 
     try {
-        const result = await fetch('https://api.manyapis.com/v1-create-short-url', {
+        // Hacer la solicitud a la nueva API sin necesidad de API Key
+        const result = await fetch('https://ulvis.net/api/v1/shorten/', {
             method: 'POST',
-            body: inputBody,
-            headers: headers
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: inputBody
         });
 
         const data = await result.json();
-        
 
         if (data && data.shortUrl) {
-            shortURL.value = data.shortUrl
+            shortURL.value = data.shortUrl;
             console.log('URL corto generado:', data.shortUrl);
             document.getElementById("shorturl").textContent = `URL corto generado: ${data.shortUrl}`;
         } else {
